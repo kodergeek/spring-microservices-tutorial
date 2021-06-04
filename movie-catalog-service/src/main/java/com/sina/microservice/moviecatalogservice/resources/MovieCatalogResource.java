e406d7abed5e2462fe14b7357edf92a8ca376612
Sina Khaknezhad <sinasthlm@yahoo.com>
2021-06-04 08:35:23 +0200

    Use eureka service name instead of url in rest calls

--- movie-catalog-service/src/main/java/com/sina/microservice/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/sina/microservice/moviecatalogservice/resources/MovieCatalogResource.java
@@ -25,12 +25,12 @@ public class MovieCatalogResource {
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 		
 		
-		UserRating ratings =  restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId,
+		UserRating ratings =  restTemplate.getForObject("http://movie-rating-service/ratingsdata/users/" + userId,
 				UserRating.class);
 				
 		return ratings.getMovieRatings().stream().map(rating -> {
 
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 			return new CatalogItem(movie.getName(), "Good movie", rating.getRating() ); 
 		})
 		.collect(Collectors.toList());
